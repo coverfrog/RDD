1c63a1c4eaa8b9385efa160aa487263a488f9e14
coverfrog <justfroglab@gmail.com>
2025-02-17 22:52:08 +0900

    Object Polling Add

---
+++ Assets/1_Scripts/Rdd/Object/Unit/Enemy/EnemyHelper.cs
@@ -0,0 +1,11 @@
+using Cf.Pattern;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class EnemyHelper : MonoBehaviour, IObjectPoolAble<EnemyHelper>
+{
+    public void SetPool(ObjectPool<EnemyHelper> pool)
+    {
+        
+    }
+}
---
+++ Assets/1_Scripts/Rdd/Object/Unit/Enemy/EnemySpawner.cs
@@ -0,0 +1,25 @@
+using Cf.Pattern;
+using UnityEngine;
+
+public class EnemySpawner : ObjectSpawner<EnemyHelper>
+{
+    protected override void OnCreateFunc(EnemyHelper obj)
+    {
+        
+    }
+
+    protected override void OnActionOnGet(EnemyHelper obj)
+    {
+        
+    }
+
+    protected override void OnActionOnRelease(EnemyHelper obj)
+    {
+        
+    }
+
+    protected override void OnActionOnDestroy(EnemyHelper obj)
+    {
+        
+    }
+}
