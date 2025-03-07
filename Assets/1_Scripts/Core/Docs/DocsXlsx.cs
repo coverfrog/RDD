b13b743b4681b8e372fc2c2364fc2951e13a0d1b
ChangSik Kim <justfroglab@gmail.com>
2025-03-07 13:06:06 +0900

    1. Xlsx, Npoi 조합 어떻게 할지 생각

--- Assets/1_Scripts/Core/Docs/DocsXlsx.cs
+++ Assets/1_Scripts/Core/Docs/DocsXlsx.cs
@@ -1,19 +1,22 @@
-using UnityEngine;
+using System.IO;
 
 namespace Cf.Docs
 {
-    public class DocsXlsx : MonoBehaviour
+    public class DocsXlsx<T> :Docs<T> where T : class, new()
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public DocsXlsx(DocsRoot docsRoot, string[] subPathArr, string fileName, DocsExtend extend, bool isCreateAuto = true) : base(docsRoot, subPathArr, fileName, extend, isCreateAuto)
         {
-        
+            
         }
 
-        // Update is called once per frame
-        void Update()
+        protected override string CreateDocsData(T t)
         {
-        
+            return "";
+        }
+
+        protected override T ReadDocsFile()
+        {
+            return null;
         }
     }
 }
